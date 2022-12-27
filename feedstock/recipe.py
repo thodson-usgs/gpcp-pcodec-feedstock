0eac29f5dfaf0eddad6c76539e19a7cd85414bd0
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-27 09:57:08 -0800

    rewrite for beam-refactor

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,6 +1,8 @@
+import apache_beam as beam
 import pandas as pd
-from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
-from pangeo_forge_recipes.recipes import XarrayZarrRecipe
+
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, prune_pattern
+from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
 
 dates = [
     d.to_pydatetime().strftime('%Y%m%d')
@@ -15,8 +17,25 @@ def make_url(time):
 concat_dim = ConcatDim("time", dates, nitems_per_file=1)
 pattern = FilePattern(make_url, concat_dim)
 
-recipe = XarrayZarrRecipe(
-    pattern,
-    inputs_per_chunk=200,
-    xarray_open_kwargs={"decode_coords": "all"}
+
+# FIXME[1]: How is `--prune` going to work in runner?
+# We can't have contributors calling `prune_pattern` themselves...
+pattern_pruned = prune_pattern(pattern)
+
+# FIXME[2]: Dynamic assignment of `target_path` (and `cache_path`)?
+from tempfile import TemporaryDirectory
+td = TemporaryDirectory()
+target_path = td.name + "/output.zarr"
+
+recipe = (
+    # FIXME[1]: `pattern_pruned`
+    beam.Create(pattern_pruned.items())
+    | OpenURLWithFSSpec()
+    # FIXME[1]: `pattern_pruned`
+    | OpenWithXarray(file_type=pattern_pruned.file_type)
+    | StoreToZarr(
+        # FIXME[2]: `target_path`
+        target_url=target_path,
+        combine_dims=pattern.combine_dim_keys,
+    )
 )
