20c229b472fe8eb362010a8d677b1c4cb73da0bf
YuviPanda <yuvipanda@gmail.com>
2022-12-27 23:00:02 -0800

    Get rid of prune pattern

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,7 +1,7 @@
 import apache_beam as beam
 import pandas as pd
 
-from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, prune_pattern
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
 
 dates = [
@@ -18,10 +18,6 @@ concat_dim = ConcatDim("time", dates, nitems_per_file=1)
 pattern = FilePattern(make_url, concat_dim)
 
 
-# FIXME[1]: How is `--prune` going to work in runner?
-# We can't have contributors calling `prune_pattern` themselves...
-pattern_pruned = prune_pattern(pattern)
-
 # FIXME[2]: Dynamic assignment of `target_path` (and `cache_path`)?
 # from tempfile import TemporaryDirectory
 # td = TemporaryDirectory()
@@ -29,10 +25,10 @@ target_path = "output.zarr"
 
 recipe = (
     # FIXME[1]: `pattern_pruned`
-    beam.Create(pattern_pruned.items())
+    beam.Create(pattern.items())
     | OpenURLWithFSSpec()
     # FIXME[1]: `pattern_pruned`
-    | OpenWithXarray(file_type=pattern_pruned.file_type)
+    | OpenWithXarray(file_type=pattern.file_type)
     | StoreToZarr(
         # FIXME[2]: `target_path`
         target_url=target_path,
